173cfc322683b11c8aa4e045e184f249e0e582e8
dpx-x <deepakkrishna2206@gmail.com>
2025-01-22 20:41:22 +0530

    Added cpp file for auto keyword

--- 1.Basics/auto.cpp
+++ 1.Basics/auto.cpp
@@ -17,15 +17,14 @@ int main() {
     auto var8 {10ll}; // long long
     auto var9 {10ul}; // unsigned long
 
-    cout << "var1 : " << var1 << endl;
-    cout << "var2 : " << var2 << endl;
-    cout << "var3 : " << var3 << endl;
-    cout << "var4 : " << var4 << endl;
-    cout << "var5 : " << var5 << endl;
-    cout << "var6 : " << var6 << endl;
-    cout << "var7 : " << var7 << endl;
-    cout << "var8 : " << var8 << endl;
-    cout << "var9 : " << var9 << endl;
-
+    cout << "var1 : " << sizeof(var1) << " Bytes" << endl;
+    cout << "var2 : " << sizeof(var2) << " Bytes" << endl;
+    cout << "var3 : " << sizeof(var3) << " Bytes" << endl;
+    cout << "var4 : " << sizeof(var4) << " Bytes" << endl;
+    cout << "var5 : " << sizeof(var5) << " Bytes" << endl;
+    cout << "var6 : " << sizeof(var6) << " Bytes" << endl;
+    cout << "var7 : " << sizeof(var7) << " Bytes" << endl;
+    cout << "var8 : " << sizeof(var8) << " Bytes" << endl;
+    cout << "var9 : " << sizeof(var9) << " Bytes" << endl;
 
 }
